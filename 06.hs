64f42800c668a189b959a2246b357c6f5e16b22a
Tom Wadeson <tom@tomwadeson.com>
2016-03-23 07:54:03 +0000

    Pure algorithm implemented for day 6's problem

--- 06.hs
+++ 06.hs
@@ -10,15 +10,29 @@ data Range = Range Coordinate Coordinate
 
 type Coordinate = (Int, Int)
 
-type Grid = [[Bool]]
+type Bulb = Bool
+
+type Grid = [[Bulb]]
+
+mkGrid :: Int -> Int -> Grid
+mkGrid w h = replicate h . replicate w $ False
 
 modifyLights :: Grid -> Instruction -> Grid
-modifyLights g i = map snd . map doRow $ zip g [0..]
+modifyLights g i = map doRow $ zip g [0..]
   where
-    doRow (row, x) = undefined
+    doRow (row, x) = fst . foldr (\b (acc, y) ->
+                             let b' = apply i (x, y) b
+                                 y' = y+1
+                             in  (b' : acc, y')) ([], 0) $ row
+
+apply :: Instruction -> Coordinate -> Bulb -> Bulb
+apply (Toggle r) c  = if inRange r c then not else id
+apply (TurnOn r) c  = if inRange r c then const True else id
+apply (TurnOff r) c = if inRange r c then const False else id
 
 inRange :: Range -> Coordinate -> Bool
-inRange (Range x y) c = c >= x && x <= y  
+inRange (Range (x1, y1) (x2, y2)) (cx, cy) =
+  cx >= x1 && cx <= x2 && cy >= y1 && cy <= y2
 
 turnedOn :: Grid -> Int
 turnedOn = countProp (== True)
