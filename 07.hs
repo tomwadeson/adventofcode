7c1a16c5d879e5675f5fc8ae72bc22dcc62b7905
Tom Wadeson <tom@tomwadeson.com>
2016-03-30 21:43:04 +0100

    Solve day 7

--- 07.hs
+++ 07.hs
@@ -1,53 +1,56 @@
 module Day7 where
 
-import qualified Data.Map.Strict as Map
-import qualified Data.Set as Set
-import Data.Foldable (foldl')
-import Data.List (delete)
-import Control.Applicative ((<$>), (<*>), (<|>))
-import Data.Maybe (fromJust)
-import Data.Bits ((.&.), (.|.), complement, shiftL, shiftR)
-import Text.ParserCombinators.ReadP
-import Data.Char (isDigit, isLetter)
 import Data.Word
+import Text.ParserCombinators.ReadP
+import Control.Applicative ((<|>))
+import Data.Char (isLetter, isDigit)
+import Data.Foldable (foldl')
+import Data.Maybe (catMaybes, maybeToList, fromJust)
+import Data.List (delete)
+import Debug.Trace (trace)
+import Data.Bits ((.&.), (.|.), complement, shiftL, shiftR)
+import qualified Data.Set as Set
+import qualified Data.Map.Strict as Map
 
-data Node = Signal { label :: Label, value :: Word16 }
-          | Operator { label :: Label, gate :: Gate }
+data Node = Wire { input :: Input, label :: Label }
+          | Operator { gate :: Gate, label ::  Label }
           deriving (Show)
 
-data Gate = And Label Label
-          | Or Label Label
-          | Not Label
-          | LShift Label Int
-          | RShift Label Int
+data Gate = And Input Input
+          | Or Input Input
+          | Not Input
+          | LShift Int Input
+          | RShift Int Input
           deriving (Show)
+
+data Input = Output Label
+           | Signal Word16
+           deriving (Show)
 
 newtype Label = Label String deriving (Eq, Ord, Show)
 
-type NodeTable = Map.Map Label Node
-
 type Graph = Map.Map Label [Label]
 
---nodes :: [Node]
---nodes = [Signal (Label "x") 1, Signal (Label "y") 2, Operator (Label "z") (And (Label "x") (Label "y"))]
+type NodeTable = Map.Map Label Node
+
+toGraph :: [Node] -> Graph
+toGraph = foldl' (\acc x -> Map.insertWith (++) (label x) (inputs x) acc) Map.empty
 
 toNodeTable :: [Node] -> NodeTable
 toNodeTable = foldl' (\acc x -> Map.insert (label x) x acc) Map.empty
 
-toGraph :: [Node] -> Graph
-toGraph nodes = toGraph' nodes Map.empty
-  where
-    toGraph' [] c     = c
-    toGraph' (n:ns) c = case n of
-      (Signal l _)   -> toGraph' ns (Map.insert l [] c)
-      (Operator l g) -> toGraph' ns (Map.insertWith (++) l (inputs g) c)
+inputs :: Node -> [Label]
+inputs (Wire i l)                = maybeToList $ output i
+inputs (Operator (And i1 i2) _)  = catMaybes $ map output [i1, i2]
+inputs (Operator (Or i1 i2) _)   = catMaybes $ map output [i1, i2]
+inputs (Operator (Not i) _)      = maybeToList $ output i
+inputs (Operator (LShift _ i) _) = maybeToList $ output i
+inputs (Operator (RShift _ i) _) = maybeToList $ output i
 
-inputs :: Gate -> [Label]
-inputs (And x y)    = [x, y]
-inputs (Or x y)     = [x, y]
-inputs (Not x)      = [x]
-inputs (LShift x _) = [x]
-inputs (RShift x _) = [x]
+-- TODO: I don't like this name
+output :: Input -> Maybe Label
+output (Output l) = Just l
+output _          = Nothing
 
 -- Topological sort, using Kahn's algorithm.  Messy, unsafe, and inefficient implementation!
 -- See: https://en.wikipedia.org/wiki/Topological_sorting#Kahn.27s_algorithm
@@ -56,124 +59,124 @@ topsort g = topsort' g (roots g) []
   where
     topsort' g s l
       | null s    = l
-      | otherwise = 
-          let n              = Set.elemAt 0 s
-              l'             = l ++ [n]
-              (g', newRoots) = removeEdgesFrom n g
-              s'             = Set.delete n $ Set.union s newRoots
-          in  topsort' g' s' l'
+      | otherwise = let n        = Set.elemAt 0 s
+                        l'       = l ++ [n]
+                        newRoots = Map.keysSet $ Map.filter (== [n]) g
+                        s'       = Set.delete n $ Set.union s newRoots
+                        g'       = Map.map (delete n) g
+                    in  topsort' g' s' l'
     
     roots = Map.keysSet . Map.filter null
 
-    removeEdgesFrom l g =
-      let newRoots = Map.keysSet $ Map.filter (== [l]) g
-          g'       = Map.map (delete l) g
-      in  (g', newRoots)
+runCircuit :: [Node] -> Map.Map Label Word16
+runCircuit nodes = let table              = toNodeTable nodes
+                       dependencyOrdering = map (fromJust . (`Map.lookup` table)) . topsort . toGraph $ nodes
+                   in  foldl' (\acc x -> execute x acc) Map.empty dependencyOrdering
 
-runCircuit :: Graph -> NodeTable -> Map.Map Label Word16
-runCircuit g nt = let nodes = map (\x -> fromJust (Map.lookup x nt)) . topsort $  g
-                  in  foldl' (\acc x -> execute x acc) Map.empty nodes
-
--- TODO: refactor the duplication; this doesn't scale well
 execute :: Node -> Map.Map Label Word16 -> Map.Map Label Word16
-execute (Signal l v) r           = Map.insert l v r
-execute (Operator l (And x y)) r = 
-  let xVal = Map.lookup x r
-      yVal = Map.lookup y r
-      z    = (.&.) <$> xVal <*> yVal
-  in  Map.insert l (fromJust z) r
-execute (Operator l (Or x y))  r = 
-  let xVal = Map.lookup x r
-      yVal = Map.lookup y r
-      z    = (.|.) <$> xVal <*> yVal
-  in  Map.insert l (fromJust z) r
-execute (Operator l (Not x ))  r = 
-  let xVal = Map.lookup x r
-      z    = (complement) <$> xVal
-  in  Map.insert l (fromJust z) r
-execute (Operator l (LShift x y))  r = 
-  let xVal = Map.lookup x r
-      z    = (`shiftL` y) <$> xVal
-  in  Map.insert l (fromJust z) r
-execute (Operator l (RShift x y))  r = 
-  let xVal = Map.lookup x r
-      z    = (`shiftR` y) <$> xVal
-  in  Map.insert l (fromJust z) r
+execute (Wire i l) m                 = Map.insert l (value i m) m
+execute (Operator (And i1 i2) l) m   = Map.insert l ((value i1 m) .&. (value i2 m)) m
+execute (Operator (Or i1 i2) l) m    = Map.insert l ((value i1 m) .|. (value i2 m)) m
+execute (Operator (Not i1) l) m      = Map.insert l (complement (value i1 m)) m
+execute (Operator (LShift s i1) l) m = Map.insert l (shiftL (value i1 m) s) m
+execute (Operator (RShift s i1) l) m = Map.insert l (shiftR (value i1 m) s) m
+
+value :: Input -> Map.Map Label Word16 -> Word16
+value (Output l) m = fromJust $ Map.lookup l m
+value (Signal s) _ = s
 
 parseNodes :: ReadP [Node]
-parseNodes = sepBy (parseSignal <|> parseAnd <|> parseOr <|> parseNot <|> parseLShift <|> parseRShift) $ satisfy (`elem` "\r\n")
+parseNodes = sepBy parseNode $ satisfy (`elem` "\r\n")
 
-parseSignal :: ReadP Node
-parseSignal = do
-  v <- parseWord16
+parseNode :: ReadP Node
+parseNode = parseWire <|> parseOperator
+
+parseWire :: ReadP Node
+parseWire = do
+  input <- parseInput
   parseArrow
-  l <- parseLabel
-  return (Signal l v)
+  label <- parseLabel
+  return $ Wire input label
 
-parseAnd :: ReadP Node
+parseOperator :: ReadP Node
+parseOperator = do
+  gate  <- parseGate
+  parseArrow
+  label <- parseLabel
+  return $ Operator gate label
+
+parseGate :: ReadP Gate
+parseGate =  parseAnd
+         <|> parseOr
+         <|> parseNot
+         <|> parseLShift
+         <|> parseRShift
+
+parseAnd :: ReadP Gate
 parseAnd = do
-  x <- parseLabel
+  i1 <- parseInput
   string " AND "
-  y <- parseLabel
-  parseArrow
-  z <- parseLabel
-  return (Operator z (And x y))
+  i2 <- parseInput
+  return $ And i1 i2
 
-parseOr :: ReadP Node
+parseOr :: ReadP Gate
 parseOr = do
-  x <- parseLabel
+  i1 <- parseInput
   string " OR "
-  y <- parseLabel
-  parseArrow
-  z <- parseLabel
-  return (Operator z (Or x y))
+  i2 <- parseInput
+  return $ Or i1 i2
 
-parseNot :: ReadP Node
+parseNot :: ReadP Gate
 parseNot = do
   string "NOT "
-  x <- parseLabel
-  parseArrow
-  z <- parseLabel
-  return (Operator z (Not x))
+  i1 <- parseInput
+  return $ Not i1
 
-parseLShift :: ReadP Node
+parseLShift :: ReadP Gate
 parseLShift = do
-  x <- parseLabel
+  i1 <- parseInput
   string " LSHIFT "
-  s <- parseNumber
-  parseArrow
-  z <- parseLabel
-  return (Operator z (LShift x s))
+  shift <- parseInt
+  return $ LShift shift i1
 
-parseRShift :: ReadP Node
+parseRShift :: ReadP Gate
 parseRShift = do
-  x <- parseLabel
+  i1 <- parseInput
   string " RSHIFT "
-  s <- parseNumber
-  parseArrow
-  z <- parseLabel
-  return (Operator z (RShift x s))
+  shift <- parseInt
+  return $ RShift shift i1
 
-parseWord16 :: ReadP Word16
-parseWord16 = read <$> many1 (satisfy isDigit)
+parseInput :: ReadP Input
+parseInput = parseOutput <|> parseSignal
 
-parseNumber :: ReadP Int
-parseNumber = read <$> many1 (satisfy isDigit)
+parseSignal :: ReadP Input
+parseSignal = do
+  signal <- parseWord16
+  return $ Signal signal
+
+parseOutput :: ReadP Input
+parseOutput = do
+  label <- parseLabel
+  return $ Output label
+
+parseLabel :: ReadP Label
+parseLabel = Label <$> munch1 (isLetter)
 
 parseArrow :: ReadP String
 parseArrow = string " -> "
 
-parseLabel :: ReadP Label
-parseLabel = Label <$> munch (isLetter)
+parseWord16 :: ReadP Word16
+parseWord16 = read <$> parseDigits
+
+parseInt :: ReadP Int
+parseInt = read <$> parseDigits
+
+parseDigits :: ReadP String
+parseDigits = many1 (satisfy isDigit)
 
 main :: IO ()
 main = do
-  nodes <- (fst . last . readP_to_S parseNodes) <$> getContents
-  print nodes
-  let nodeTable = toNodeTable nodes
-  let graph     = toGraph nodes
-  let result    = runCircuit graph nodeTable
-  print result
-  putStrLn "If you're looking for 'a', it's:"
-  print $ Map.lookup (Label "a") result
+  nodes <- fst . last . readP_to_S parseNodes <$> getContents
+  let result = Map.toList $ runCircuit nodes
+  mapM_ print result
 
